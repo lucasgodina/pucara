37d9611fdfe6bfb91865ce6007030ce48928eebe
Lucas Godina <lucasgodina@gmail.com>
2025-10-18 23:09:15 -0300

    refactor: switch database from MySQL to SQLite
    - Updated .env.example to remove MySQL configuration and set DB_CONNECTION to sqlite.
    - Modified .gitignore to ignore SQLite database files.
    - Changed database configuration in database.ts to use better-sqlite3.
    - Removed MySQL2 dependency and added better-sqlite3 in package.json and package-lock.json.
    - Updated env.ts to reflect the new database connection settings.

--- backend/crud-players-pucara/config/database.ts
+++ backend/crud-players-pucara/config/database.ts
@@ -1,18 +1,15 @@
-import env from '#start/env'
+import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: 'sqlite',
   connections: {
-    mysql: {
-      client: 'mysql2',
+    sqlite: {
+      client: 'better-sqlite3',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        filename: app.makePath('database/pucara.sqlite'),
       },
+      useNullAsDefault: true,
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
